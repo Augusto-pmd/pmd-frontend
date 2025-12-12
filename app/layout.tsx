7b513b025d847d323640fecfdb3fe65875646ee7
Augusto PMD <augusto-pmd@pmd.com>
2025-12-12 01:16:58 -0300

    WRAP APP WITH AUTH PROVIDER

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/ui-system.css";
 import "../styles/motion.css";
 import "./globals.css";
 import { ToastProvider } from "@/components/ui/Toast";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function RootLayout({
   children,
@@ -11,7 +12,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ToastProvider>{children}</ToastProvider>
+        <AuthProvider>
+          <ToastProvider>{children}</ToastProvider>
+        </AuthProvider>
       </body>
     </html>
   );
